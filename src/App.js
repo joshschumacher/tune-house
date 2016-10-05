fb0805ae391f4ad7802de97daa60b00d35bcbc30
Josh Schumacher <joshs@tune.com>
2016-10-05 14:25:08 -0700

    Use Props, isolate a component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
+import Counter from './Counter.js';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0,
-    };
-  }
-
   render() {
     return (
       <div className="App">
@@ -17,12 +12,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          {this.state.counter}
-          <button onClick={() => this.setState({ counter: this.state.counter + 1 })}>Increment Counter</button>
-          <button onClick={() => this.setState({ counter: this.state.counter - 1 })}>Decrement Counter</button>
-          <button onClick={() => this.setState({ counter: 0 })}>Reset Counter</button>
-        </p>
+
+        <Counter name="Frank" counterDefault={42} />
       </div>
     );
   }
---
+++ src/Counter.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    console.log('default props', props);
+    this.state = {
+      counter: props.counterDefault,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hey {this.props.name}! The counter is currently {this.state.counter}</p>
+        <p>
+          <button onClick={() => this.setState({ counter: this.state.counter + 1 })}>Increment Counter</button>
+          <button onClick={() => this.setState({ counter: this.state.counter - 1 })}>Decrement Counter</button>
+          <button onClick={() => this.setState({ counter: this.props.counterDefault })}>Reset Counter</button>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Counter;
