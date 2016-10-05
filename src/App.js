fe2d452df185cd05ba24dfb61393b1638de4dfe6
Josh Schumacher <joshs@tune.com>
2016-10-05 13:53:52 -0700

    Use State

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +18,10 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.counter}
+          <button onClick={() => this.setState({ counter: this.state.counter + 1 })}>Increment Counter</button>
+          <button onClick={() => this.setState({ counter: this.state.counter - 1 })}>Decrement Counter</button>
+          <button onClick={() => this.setState({ counter: 0 })}>Reset Counter</button>
         </p>
       </div>
     );
